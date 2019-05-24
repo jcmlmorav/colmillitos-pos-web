bc40d382992c1e4a1a5ab56625c8dad06b55695f
Juan Camilo Mora <jcmlmorav@gmail.com>
2019-05-24 02:52:46 +0000

    Add router to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, } from 'react-router-dom';
+import 'antd/dist/antd.css';
 import Billing from './pages/Billing';
-
 
 const App: React.FC = () => {
   return (
-    <Billing />
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Billing} />
+        <Route path="/billing" component={() => <h1>Billing</h1>} />
+        <Route component={() => <h1>404 Not found</h1>} />
+      </Switch>
+    </Router>
   );
 }
 
